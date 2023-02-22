6a9cd49545ad30a1bf2ea1c328115b55c35f164d
3599josephw <3599@holbertonschool.com>
2023-02-22 10:00:10 -0600

    removed tabbing

--- 0x03-asynchronous/breaking_bad.dart
+++ 0x03-asynchronous/breaking_bad.dart
@@ -4,67 +4,67 @@ import 'dart:convert';
 
 Future<void> printBbCharacters() {
 	print("""
-		Walter White
-		Jesse Pinkman
-		Skyler White
-		Walter White Jr.
-		Henry Schrader
-		Marie Schrader
-		Mike Ehrmantraut
-		Saul Goodman
-		Gustavo Fring
-		Hector Salamanca
-		Domingo Molina
-		Tuco Salamanca
-		Marco & Leonel Salamanca
-		Lydia Rodarte-Quayle
-		Todd Alquist
-		Jane Margolis
-		Skinny Pete
-		Brandon Mayhew
-		Huell Babineaux
-		Steven Gomez
-		Theodore Beneke
-		Gale Boetticher
-		Andrea Cantillo
-		Brock Cantillo
-		Carmen Molina
-		Gretchen Schwartz
-		Elliot Schwartz
-		Gonzo
-		Christian Ortgea
-		Mrs. Pinkman
-		Adam Pinkman
-		Jake Pinkman
-		No-Doze
-		Emilio Koyama
-		Dr. Delcavoli
-		Wendy S.
-		Bogdan Wolynetz
-		Ken
-		Holly White
-		George Merkert
-		Donald Margolis
-		Clovis
-		SAC Ramey
-		Victor
-		Tomás Cantillo
-		Francesca Liddy
-		Cynthia
-		Tortuga
-		Tim Roberts
-		Juan Bolsa
-		Group Leader
-		Kaylee Ehrmantraut
-		Pamela
-		Duane Chow
-		Stacey Ehrmantraut
-		Officer Saxton
-		Jack Welker
-		Kimberly Wexler
-		Howard Hamlin
-		Charles McGill
-		Ignacio Varga
-		Eduardo Salamanca
+Walter White
+Jesse Pinkman
+Skyler White
+Walter White Jr.
+Henry Schrader
+Marie Schrader
+Mike Ehrmantraut
+Saul Goodman
+Gustavo Fring
+Hector Salamanca
+Domingo Molina
+Tuco Salamanca
+Marco & Leonel Salamanca
+Lydia Rodarte-Quayle
+Todd Alquist
+Jane Margolis
+Skinny Pete
+Brandon Mayhew
+Huell Babineaux
+Steven Gomez
+Theodore Beneke
+Gale Boetticher
+Andrea Cantillo
+Brock Cantillo
+Carmen Molina
+Gretchen Schwartz
+Elliot Schwartz
+Gonzo
+Christian Ortgea
+Mrs. Pinkman
+Adam Pinkman
+Jake Pinkman
+No-Doze
+Emilio Koyama
+Dr. Delcavoli
+Wendy S.
+Bogdan Wolynetz
+Ken
+Holly White
+George Merkert
+Donald Margolis
+Clovis
+SAC Ramey
+Victor
+Tomás Cantillo
+Francesca Liddy
+Cynthia
+Tortuga
+Tim Roberts
+Juan Bolsa
+Group Leader
+Kaylee Ehrmantraut
+Pamela
+Duane Chow
+Stacey Ehrmantraut
+Officer Saxton
+Jack Welker
+Kimberly Wexler
+Howard Hamlin
+Charles McGill
+Ignacio Varga
+Eduardo Salamanca
 	""");
 }
