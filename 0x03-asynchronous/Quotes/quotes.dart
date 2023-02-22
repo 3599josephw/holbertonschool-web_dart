4953e9d162d9f36eb708e23def020b6d8d99a4b6
3599josephw <3599@holbertonschool.com>
2023-02-22 12:25:21 -0600

    return, not print

--- 0x03-asynchronous/Quotes/quotes.dart
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -1,14 +1,19 @@
 Future<String> generateQuote(id) {
-	if (id == 1 ) {
-		print("Walter White : I am not in danger, Skyler. I am the danger!");
+	try {
+		if (id == 1 ) {
+			return("Walter White : I am not in danger, Skyler. I am the danger!");
+		}
+		if (id == 28) {
+			return("Skyler White : I'm waiting for the cancer to come back.");
+		}
+		if (id == 23) {
+			return("Jesse Pinkman : Yo 148, 3-to-the-3-to-the-6-to-the-9. Representin’ the ABQ. What up, biatch? Leave it at the tone!");
+		}
+		if (id == 2022) {
+			return("There are no quotes");
+		}
+	} catch (e) {
+		return "There are no quotes";
 	}
-	if (id == 28) {
-		print("Skyler White : I'm waiting for the cancer to come back.");
-	}
-	if (id == 23) {
-		print("Jesse Pinkman : Yo 148, 3-to-the-3-to-the-6-to-the-9. Representin’ the ABQ. What up, biatch? Leave it at the tone!");
-	}
-	if (id == 2022) {
-		print("There are no quotes");
-	}
+	
 }
