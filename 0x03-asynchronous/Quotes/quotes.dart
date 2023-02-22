b41cc52eda8e239836c0d0aaf256aeb1e8a527d2
3599josephw <3599@holbertonschool.com>
2023-02-22 12:18:18 -0600

    quotes adde

---
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -0,0 +1,7 @@
+Future<String> generateQuote(id) {
+	print("""
+Walter White : I am not in danger, Skyler. I am the danger!
+Skyler White : I'm waiting for the cancer to come back.
+Jesse Pinkman : Yo 148, 3-to-the-3-to-the-6-to-the-9. Representin’ the ABQ. What up, biatch? Leave it at the tone!
+There are no quotes""");
+}
