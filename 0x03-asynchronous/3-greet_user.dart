fd8e4a8377857b0f23675d8d6bf09e4c969133e6
3599josephw <3599@holbertonschool.com>
2023-02-22 08:57:42 -0600

    task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,23 @@
+import '3-main.dart';
+import 'dart:convert';
+
+
+Future<String> greetUser() async {
+	try {
+		var user = await fetchUserData();
+		return "Hello ${json.decode(await fetchUserData())['username']}";
+	} catch (e) {
+		return "$e";
+	}
+}
+
+Future<String> loginUser() async {
+	try {
+		if (await checkCredentials()) {
+			return await greetUser();
+		}
+		return "Wrong credentials";
+	} catch (e) {
+		return("$e");
+	}
+}
