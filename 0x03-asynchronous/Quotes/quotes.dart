8277a7229085e0bcb7eb3e9d087c4359e3b9cb2b
3599josephw <3599@holbertonschool.com>
2023-02-22 12:21:27 -0600

    if statements

--- 0x03-asynchronous/Quotes/quotes.dart
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -1,7 +1,14 @@
 Future<String> generateQuote(id) {
-	print("""
-Walter White : I am not in danger, Skyler. I am the danger!
-Skyler White : I'm waiting for the cancer to come back.
-Jesse Pinkman : Yo 148, 3-to-the-3-to-the-6-to-the-9. Representin’ the ABQ. What up, biatch? Leave it at the tone!
-There are no quotes""");
+	if (id == 1 ) {
+		print("Walter White : I am not in danger, Skyler. I am the danger!");
+	}
+	if (id == 28) {
+		print("Skyler White : I'm waiting for the cancer to come back.");
+	}
+	if (id == 23) {
+		print("Jesse Pinkman : Yo 148, 3-to-the-3-to-the-6-to-the-9. Representin’ the ABQ. What up, biatch? Leave it at the tone!");
+	}
+	if (id == 2022) {
+		print("There are no quotes");
+	}
 }
